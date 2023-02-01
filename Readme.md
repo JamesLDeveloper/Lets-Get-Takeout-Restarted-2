119315126bdcafdd6baeb83bed09f07b3be521f4
James L <jameslloyd2007@hotmail.co.uk>
2023-02-01 22:24:55 +0000

    Add foodMenu list creation and toString method. Added test for methods but not complete.

--- Readme.md
+++ Readme.md
@@ -1,1 +1,15 @@
 # Lets-Get-Takeout-Restarted-2
+
+
+## Project Information
+This project is to test generic classes, implementations, methods and maps by creating a simple ordering food java program.
+
+## Installation Instructions
+Copy all files in their directories to a chosen location.
+
+Can open project in IDE or launch from terminal
+
+FilePath/src/
+
+javac Main.java
+java Main
--- src/FoodMenu.java
+++ src/FoodMenu.java
@@ -1,4 +1,49 @@
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // Create FoodMenu class here
+
+
+public class FoodMenu{
+
+    Food food;
+
+    private List<Food> menu;
+
+    public FoodMenu(){
+
+
+        Food bakedbeans = new Food("Baked Beans", "Delicious", 3 );
+        Food katsuCurry = new Food("Chicken Katsu Curry", "Chicken in breadcrumbs with rice and sauce", 8);
+        Food steakAndChips = new Food("Steak and Chips", "Medium Rare Sirloin steak with garlic butter, double cooked chips and seasonal veg.", 29);
+
+
+ //       menu = new ArrayList<Food>();
+        menu = new ArrayList<Food>(Arrays.asList(bakedbeans, katsuCurry, steakAndChips));
+
+//menu.add(bakedbeans);
+//menu.add(katsuCurry);
+//menu.add(steakAndChips);
+//menu.add()
+
+
+    }
+
+
+    @Override
+    public String toString(){
+        StringBuilder wholeMenu = new StringBuilder();
+for (int i = 0; i < menu.size(); i++){
+
+    wholeMenu.append(menu.indexOf(i));
+    wholeMenu.append(": ");
+    wholeMenu.append(menu.get(i).toString());
+
+}
+        return wholeMenu.toString();
+    }
+
+}
