dbb35f48ddbf9d208337f92bd3fdcbd072f22ec9
James L <jameslloyd2007@hotmail.co.uk>
2023-02-02 21:52:50 +0000

    Add getFood and getlowestCostFood

--- src/FoodMenu.java
+++ src/FoodMenu.java
@@ -7,21 +7,21 @@ import java.util.List;
 // Create FoodMenu class here
 
 
-public class FoodMenu{
+public class FoodMenu {
 
     Food food;
 
     private List<Food> menu;
 
-    public FoodMenu(){
+    public FoodMenu() {
 
 
-        Food bakedbeans = new Food("Baked Beans", "Delicious", 3 );
+        Food bakedbeans = new Food("Baked Beans", "Delicious", 3);
         Food katsuCurry = new Food("Chicken Katsu Curry", "Chicken in breadcrumbs with rice and sauce", 8);
         Food steakAndChips = new Food("Steak and Chips", "Medium Rare Sirloin steak with garlic butter, double cooked chips and seasonal veg.", 29);
 
 
- //       menu = new ArrayList<Food>();
+        //       menu = new ArrayList<Food>();
         menu = new ArrayList<Food>(Arrays.asList(bakedbeans, katsuCurry, steakAndChips));
 
 //menu.add(bakedbeans);
@@ -33,17 +33,50 @@ public class FoodMenu{
     }
 
 
+//    @Override
+//    public String toString(){
+//        StringBuilder wholeMenu = new StringBuilder();
+//for (int i = 0; i < menu.size(); i++){
+//
+//    wholeMenu.append(menu.indexOf(i));
+//    wholeMenu.append(": ");
+//    wholeMenu.append(menu.get(i).toString());
+//
+//}
+//        return wholeMenu.toString();
+//    }
+
+
     @Override
-    public String toString(){
-        StringBuilder wholeMenu = new StringBuilder();
-for (int i = 0; i < menu.size(); i++){
+    public String toString() {
+        String wholeMenu = "";
+        for (int i = 0; i < menu.size(); i++) {
 
-    wholeMenu.append(menu.indexOf(i));
-    wholeMenu.append(": ");
-    wholeMenu.append(menu.get(i).toString());
+            wholeMenu = menu.indexOf(i) + ": " + menu.get(i).toString();
 
-}
-        return wholeMenu.toString();
+        }
+        return wholeMenu;
+    }
+
+    public Food getFood(int index) {
+        if (menu.get(index - 1) != null) {
+
+            return menu.get(index - 1);
+        } else {
+            return null;
+        }
+    }
+
+    public Food getLowestCostFood() {
+        int lowestCostFoodCost = menu.get(0).getPrice();
+        Food lowestCostFood = menu.get(0);
+        for (Food foodItem : menu) {
+            if (foodItem.getPrice() < lowestCostFoodCost) {
+                lowestCostFood = foodItem;
+                lowestCostFoodCost = foodItem.getPrice();
+            }
+        }
+        return lowestCostFood;
     }
 
 }
